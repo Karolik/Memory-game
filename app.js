ab5979767cd26ed6f193a87400cb638ea445042c
Karolik <36989581+Karolik@users.noreply.github.com>
2018-04-02 15:47:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
- const cards = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb", "fa fa-anchor", "fa fa-anchor" ]
+const cards = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb", "fa fa-anchor", "fa fa-anchor" ]
 
 /*
  * Display the cards on the page
@@ -9,8 +9,8 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
- // Shuffle function from http://stackoverflow.com/a/2450976
- 
+
+// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -24,48 +24,10 @@ function shuffle(array) {
 
     return array;
 }
-shuffle(cards);
 
-//Loop through each card and create its HTML;  - Add each card's HTML to the page
-//Store the HTML in a string
-
-let cardGrid = "";
-
-for(const card of cards) {
-    cardGrid += '<li class="card"><i class="'+card+'"></i></li>'; 
-}
-
-const deck = document.querySelector('.deck');
-deck.insertAdjacentHTML('afterbegin', cardGrid);
-
-
-/* event listener - a card is clicked -display the card's symbol (put this functionality in another function that you call from this one) */
-
-function addClassShow() {
-   cards.classList.add("show");
-}
-for (let i = 0 ; i < cards.length; i++) {
-  cards[i].addEventListener('click' , addClassShow()) ; 
-  }
-
-
-
-//function to restart the game
+//Function to restart the game
 
 function initialize(){
-    function shuffle(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
-    
-        while (currentIndex !== 0) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-        }
-    
-        return array;
-    }
     shuffle(cards);
 
     const restart = document.querySelector('.restart');
@@ -73,13 +35,25 @@ function initialize(){
         return this;
       });
 
-    //add cards to the page
+    //Loop through each card and create its HTML;  - Add each card's HTML to the page (Store the HTML in a string)
     let cardGrid = "";
     for(const card of cards) {
         cardGrid += '<li class="card"><i class="'+card+'"></i></li>'; 
     }
     const deck = document.querySelector('.deck');
-    deck.insertAdjacentHTML('afterbegin', cardGrid);
-  
+    deck.insertAdjacentHTML('afterbegin', cardGrid); 
 }
+window.onload = initialize();
 
+/* event listener - a card is clicked -display the card's symbol (put this functionality in another function that you call from this one) */
+//const card = document.getElementsByClassName('card');
+
+function addClassShow(event) {
+    const card = document.getElementsByClassName('card');
+    for (let i = 0 ; i < card.length; i++) {
+   card[i].classList.add('show','open');
+}
+};
+
+const deck = document.querySelector('.deck');
+deck.addEventListener('click', addClassShow(event));
